de89ddca79a866d574e929c32d99454e7b063468
Aleksandra <a.haniok@gmail.com>
2022-07-10 11:51:54 +0000

    Add Routing and connect to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,27 +1,30 @@
-import React from 'react';
-import { Navbar, Container, Nav } from 'react-bootstrap';
-import logo from '../assets/SnapFood-logo.png'
-import styles from '../styles/NavBar.module.css'
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from "../assets/SnapFood-logo.png";
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        <Navbar.Brand>
+        <NavLink to="/">
+          <Navbar.Brand>
             <img src={logo} alt="SnapFood logo" height="60" width="90" />
-        </Navbar.Brand>
+          </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-right">
-            <Nav.Link>
-                <i class="fa-solid fa-house"></i>Home
-            </Nav.Link>
-            <Nav.Link>
-                <i class="fa-solid fa-user-plus"></i>Sign up
-            </Nav.Link>
-            <Nav.Link>
-                <i class="fa-solid fa-right-to-bracket"></i>Log in
-            </Nav.Link>
+            <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/">
+              <i className="fa-solid fa-house"></i>Home
+            </NavLink>
+            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signup">
+              <i className="fa-solid fa-user-plus"></i>Sign up
+            </NavLink>
+            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/login">
+              <i className="fa-solid fa-right-to-bracket"></i>Log in
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
