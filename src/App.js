2bfaf9404b57471e32a6f269bb6b1d7631cfe3d0
Aleksandra <a.haniok@gmail.com>
2022-07-15 15:23:36 +0000

    Add form to create new posts

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Landing from "./components/Landing";
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import LogInForm from "./pages/auth/LogInForm";
+import PostCreateForm from "./pages/posts/PostCreateForm";
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
               <Route exact path="/" render={() => <Landing />} />
               <Route exact path="/signup" render={() => <SignUpForm />} />
               <Route exact path="/login" render={() => <LogInForm />} />
+              <Route exact path="/posts/create" render={() => <PostCreateForm /> } />
               <Route render={() => <h3>Ooops, this page was not found!</h3>} />
             </Switch>
           </Container>
---
+++ src/pages/posts/PostCreateForm.js
@@ -0,0 +1,89 @@
+import React from "react";
+import { Button, Col, Container, Form, Image, Row } from "react-bootstrap";
+import Upload from "../../assets/upload-image.png";
+import styles from "../../styles/PostCreateEditForm.module.css";
+import appStyles from "../../App.module.css";
+import inputStyles from "../../styles/LogInSignUpForm.module.css";
+
+function PostCreateForm() {
+  const textFields = (
+    <div className="text-center">
+      <Form.Group>
+        <Form.Label>Title</Form.Label>
+        <Form.Control type="text" name="title" className={inputStyles.Input} />
+      </Form.Group>
+
+      <Form.Group>
+        <Form.Label>Category</Form.Label>
+        <Form.Control as="select">
+          <option>Select type of cousine</option>
+          <option value="spanish">Spanish</option>
+          <option value="polish">Polish</option>
+          <option value="greek">Greek</option>
+          <option value="italian">Italian</option>
+          <option value="turkish">Turkish</option>
+          <option value="french">French</option>
+          <option value="moroccan">Moroccan</option>
+          <option value="british">British</option>
+          <option value="german">German</option>
+          <option value="austrian">Austrian</option>
+          <option value="lebanese">Lebanese</option>
+          <option value="caribbean">Caribbean</option>
+          <option value="indian">Indian</option>
+          <option value="thai">Thai</option>
+          <option value="japanese">Japanese</option>
+          <option value="chinese">Chinese</option>
+          <option value="mexican">Mexican</option>
+          <option value="american">American</option>
+          <option value="other">Other</option>
+        </Form.Control>
+      </Form.Group>
+
+      <Form.Group>
+        <Form.Label>Description</Form.Label>
+        <Form.Control
+          as="textarea"
+          rows={8}
+          name="description"
+          className={inputStyles.Input}
+        />
+      </Form.Group>
+
+      <Button className={appStyles.button} type="submit">
+        Create
+      </Button>
+
+      <Button className={`${appStyles.button} mx-3`} onClick={() => {}}>
+        Cancel
+      </Button>
+    </div>
+  );
+
+  return (
+    <Form>
+      <Row>
+        <Col md={7} lg={8} className="d-none d-md-block p-0 p-md-2">
+          <Container className={appStyles.Content}>{textFields}</Container>
+        </Col>
+        <Col className="py-2 p-0 p-md-2" md={5} lg={4}>
+          <Container
+            className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
+          >
+            <Form.Group className="text-center">
+              <Form.Label
+                className="d-flex justify-content-center"
+                htmlFor="image-upload"
+              >
+                    <img src={Upload} alt="Upload image" />
+              </Form.Label>
+            </Form.Group>
+
+            <div className="d-md-none">{textFields}</div>
+          </Container>
+        </Col>
+      </Row>
+    </Form>
+  );
+}
+
+export default PostCreateForm;
