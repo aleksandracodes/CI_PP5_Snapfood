174ab2b63f492e8dbc84b421dba4a992979cbc4a
Aleksandra <a.haniok@gmail.com>
2022-07-16 15:44:56 +0000

    Add API request to fetch post by id

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -1,23 +1,50 @@
-import React from "react";
-
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import Container from "react-bootstrap/Container";
-
+import React, { useEffect, useState } from "react";
+import { Row, Col, Container } from "react-bootstrap";
 import appStyles from "../../App.module.css";
+import { useParams } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
 
 function PostPage() {
-  // Add your logic here
+  const { id } = useParams();
+  const [post, setPost] = useState({ results: [] });
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const [{ data: post }] = await Promise.all([
+          axiosReq.get(`/posts/${id}`),
+        ]);
+        setPost({ results: [post] });
+        console.log(post);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    handleMount();
+  }, [id]); // run code every time the post id in the url changes
 
   return (
     <Container>
       <Row>
         <Col className="py-2 p-0 p-lg-2" lg={4}>
-          <Container className={`${appStyles.Content} ${appStyles.CollapsedColumn} mb-3`}>Like, feed, add</Container>
+          <Container
+            className={`${appStyles.Content} ${appStyles.CollapsedColumn} mb-3`}
+          >
+            Like, feed, add
+          </Container>
 
-          <Container className={`${appStyles.Content} ${appStyles.CollapsedColumn} mb-3`}>Top 3 users</Container>
+          <Container
+            className={`${appStyles.Content} ${appStyles.CollapsedColumn} mb-3`}
+          >
+            Top 3 users
+          </Container>
 
-          <Container className={`${appStyles.Content} ${appStyles.CollapsedColumn} mb-3`}>Post category</Container>
+          <Container
+            className={`${appStyles.Content} ${appStyles.CollapsedColumn} mb-3`}
+          >
+            Post category
+          </Container>
         </Col>
 
         <Col className="py-2 p-0 p-lg-2" lg={8}>
