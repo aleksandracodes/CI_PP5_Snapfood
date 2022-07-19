0bcc39102b7bcff99f09aae962f29598a8876979
Aleksandra <a.haniok@gmail.com>
2022-07-19 14:51:26 +0000

    Add a search bar for posts by a username or a title

--- src/pages/posts/MainPostsPage.js
+++ src/pages/posts/MainPostsPage.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Form, Row } from "react-bootstrap";
+import styles from "../../styles/MainPostsPage.module.css";
 import appStyles from "../../App.module.css";
 import Post from "./Post";
 import Asset from "../../components/Asset";
@@ -13,20 +14,22 @@ function MainPostsPage({ message, filter = "" }) {
   const [hasLoaded, setHasLoaded] = useState(false);
   const { pathname } = useLocation();
 
+  const [query, setQuery] = useState("");
+
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const { data } = await axiosReq.get(`/posts/?${filter}`);
+        const { data } = await axiosReq.get(`/posts/?${filter}search=${query}`);
         setPosts(data);
         setHasLoaded(true);
       } catch (err) {
-        // console.log(err);
+        console.log(err);
       }
     };
 
     setHasLoaded(false);
     fetchPosts();
-  }, [filter, pathname]);
+  }, [filter, query, pathname]);
 
   return (
     <Container>
@@ -48,6 +51,20 @@ function MainPostsPage({ message, filter = "" }) {
         </Col>
 
         <Col className="py-2 p-0 p-lg-2" lg={8}>
+
+        <i className={`fas fa-search ${styles.SearchIcon}`} />
+        <Form
+          className={styles.SearchBar}
+          onSubmit={(e) => e.preventDefault()}
+        >
+          <Form.Control
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            type="text"
+            className="mr-sm-2"
+            placeholder="Search posts"
+          />
+        </Form>
 
         {hasLoaded ? (
           <>
