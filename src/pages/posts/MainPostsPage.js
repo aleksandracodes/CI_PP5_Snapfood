21e7f0800b626d3e9e7f9bf9a43548f8b1707093
Aleksandra <a.haniok@gmail.com>
2022-07-19 12:57:48 +0000

    Display posts to home, feed and liked pages. Make filtered requests to API.

--- src/pages/posts/MainPostsPage.js
+++ src/pages/posts/MainPostsPage.js
@@ -1,10 +1,33 @@
-import React from "react";
-import { Col, Container, Form, Row } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
 import appStyles from "../../App.module.css";
-import styles from "../../styles/MainPostsPage.module.css";
+import Post from "./Post";
+import Asset from "../../components/Asset";
+import NoResultsImage from "../../assets/no-results-found.png";
 import LikeFeedAddPost from "../../components/LikeFeedAddPost";
+import { useLocation } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
 
-function PostsPage() {
+function MainPostsPage({ message, filter = "" }) {
+  const [posts, setPosts] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const { data } = await axiosReq.get(`/posts/?${filter}`);
+        setPosts(data);
+        setHasLoaded(true);
+      } catch (err) {
+        // console.log(err);
+      }
+    };
+
+    setHasLoaded(false);
+    fetchPosts();
+  }, [filter, pathname]);
+
   return (
     <Container>
       <Row>
@@ -25,10 +48,29 @@ function PostsPage() {
         </Col>
 
         <Col className="py-2 p-0 p-lg-2" lg={8}>
+
+        {hasLoaded ? (
+          <>
+            {posts.results.length ? (
+              posts.results.map((post) => (
+                <Post key={post.id} {...post} setPosts={setPosts} />
+              ))
+            ) : (
+              <Container className={appStyles.Content}>
+                <Asset src={NoResultsImage} width={20} height={20} message={message} />
+              </Container>
+            )}
+          </>
+        ) : (
+          <Container className={appStyles.Content}>
+            <Asset spinner />
+          </Container>
+        )}
+
         </Col>
       </Row>
     </Container>
   );
 }
 
-export default PostsPage;
+export default MainPostsPage;
