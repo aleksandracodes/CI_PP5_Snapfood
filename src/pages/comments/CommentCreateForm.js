5c81e48747470cbae24dc3ffddb740b6ce832b95
Aleksandra <a.haniok@gmail.com>
2022-07-20 15:23:32 +0000

    Add and style the comment form and link to post page

---
+++ src/pages/comments/CommentCreateForm.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import {Button, Form, InputGroup } from "react-bootstrap";
+import styles from "../../styles/CommentCreateEditForm.module.css";
+import appStyles from "../../App.module.css";
+import Avatar from "../../components/Avatar";
+import { axiosRes } from "../../api/axiosDefaults";
+
+function CommentCreateForm(props) {
+  const { post, setPost, setComments, profileImage, profile_id } = props;
+  const [content, setContent] = useState("");
+
+  const handleChange = (e) => {
+    setContent(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axiosRes.post("/comments/", {
+        content,
+        post,
+      });
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: [data, ...prevComments.results],
+      }));
+      setPost((prevPost) => ({
+        results: [
+          {
+            ...prevPost.results[0],
+            comments_count: prevPost.results[0].comments_count + 1,
+          },
+        ],
+      }));
+      setContent("");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <Form className="mt-2 text-center" onSubmit={handleSubmit}>
+      <Form.Group>
+        <InputGroup>
+          <p className="my-2">
+            <Link to={`/profiles/${profile_id}`}>
+              <Avatar src={profileImage} />
+            </Link>
+          </p>
+          <Form.Control
+            className={styles.Form}
+            placeholder="share what you think..."
+            as="textarea"
+            value={content}
+            onChange={handleChange}
+            rows={2}
+          />
+        </InputGroup>
+      </Form.Group>
+
+      <Button 
+        className={appStyles.button}
+        type="submit"
+      >
+        Add
+      </Button>
+    </Form>
+  );
+}
+
+export default CommentCreateForm;
--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -4,10 +4,16 @@ import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
+import CommentCreateForm from "../comments/CommentCreateForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 function PostPage() {
   const { id } = useParams();
   const [post, setPost] = useState({ results: [] });
+
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const [comments, setComments] = useState({ results: [] });
 
   useEffect(() => {
     const handleMount = async () => {
@@ -50,7 +56,19 @@ function PostPage() {
 
         <Col className="py-2 p-0 p-lg-2" lg={8}>
         <Post {...post.results[0]} setPosts={setPost} postPage />
-          <Container className={appStyles.Content}>Comments</Container>
+          <Container className={appStyles.Content}>
+          {currentUser ? (
+            <CommentCreateForm
+              profile_id={currentUser.profile_id}
+              profileImage={profile_image}
+              post={id}
+              setPost={setPost}
+              setComments={setComments}
+            />
+          ) : comments.results.length ? (
+            "Comments"
+          ) : null}
+          </Container>
         </Col>
       </Row>
     </Container>
