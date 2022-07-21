21bf9aa5253b51542b710e4d8d130e766ce24866
Aleksandra <a.haniok@gmail.com>
2022-07-21 10:27:59 +0000

    Add the dropdown menu component to comments page

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -3,9 +3,14 @@ import { Media } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
 import styles from "../../styles/Comment.module.css";
+import { DropdownMenu } from "../../components/DropdownMenu";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 const Comment = (props) => {
   const { profile_id, profile_image, owner, updated_on, content } = props;
+
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
 
   return (
     <div>
@@ -17,9 +22,15 @@ const Comment = (props) => {
           <div className={styles.CommentBox}>
             <span className={styles.OwnerName}>{owner}</span>
             <span className={styles.Date}> | {updated_on}</span>
+            <span className={styles.DropdownDots}>
+            {is_owner && (
+              <DropdownMenu handleEdit={() => {}} handleDelete={() => {}} />
+              )}
+            </span>
             <p className="pr-2 pt-2">{content}</p>
           </div>
         </Media.Body>
+
       </Media>
     </div>
   );
