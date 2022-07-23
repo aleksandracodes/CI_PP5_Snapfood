818da2c1db67869d1a75cf7329f084bae6270924
Aleksandra <a.haniok@gmail.com>
2022-07-23 16:52:41 +0000

    Add functionality of post filtering by its category

--- src/pages/posts/MainPostsPage.js
+++ src/pages/posts/MainPostsPage.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Col, Container, Form, Row } from "react-bootstrap";
+import { Badge, Col, Container, Form, Row } from "react-bootstrap";
 import styles from "../../styles/MainPostsPage.module.css";
 import appStyles from "../../App.module.css";
 import Post from "./Post";
@@ -11,23 +11,28 @@ import { axiosReq } from "../../api/axiosDefaults";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
 import PopularProfiles from "../profiles/PopularProfiles";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 function MainPostsPage({ message, filter = "" }) {
   const [posts, setPosts] = useState({ results: [] });
+  const [category, setCategory] = useState(null);
   const [hasLoaded, setHasLoaded] = useState(false);
   const { pathname } = useLocation();
-
+  const currentUser = useCurrentUser();
   const [query, setQuery] = useState("");
 
   useEffect(() => {
     const fetchPosts = async () => {
-      try {
-        const { data } = await axiosReq.get(`/posts/?${filter}search=${query}`);
-        setPosts(data);
-        setHasLoaded(true);
-      } catch (err) {
-        console.log(err);
-      }
+        try {
+            const { data } = await axiosReq.get(
+                `/posts/?${filter}search=${query}${
+                    category !== null ? `&category=${category}` : ""
+                }`
+            );
+            setPosts(data);
+            setHasLoaded(true);
+        } catch (err) {
+        }
     };
 
     setHasLoaded(false);
@@ -38,7 +43,7 @@ function MainPostsPage({ message, filter = "" }) {
     return () => {
       clearTimeout(timer);
     };
-  }, [filter, query, pathname]);
+  }, [filter, query, pathname, currentUser, category]);
 
   return (
     <Container>
@@ -55,7 +60,27 @@ function MainPostsPage({ message, filter = "" }) {
           <Container
             className={`${appStyles.Content} ${appStyles.CollapsedColumn} mb-2`}
           >
-            Post category
+            <p className=" font-weight-bold ml-2">Post categories</p>
+              <Badge variant="secondary" pill className={`${styles.Badge}`} onClick={() => setCategory(null)}>All</Badge>     
+              <Badge variant="secondary" pill className={`${styles.Badge}`} onClick={() => setCategory("spanish")}>Spanish</Badge>             
+              <Badge variant="secondary" pill className={`${styles.Badge}`} onClick={() => setCategory("polish")}>Polish</Badge>
+              <Badge variant="secondary" pill className={`${styles.Badge}`} onClick={() => setCategory("italian")}>Italian</Badge>
+              <Badge variant="secondary" pill className={`${styles.Badge}`} onClick={() => setCategory("greek")}>Greek</Badge>
+              <Badge variant="secondary" pill className={`${styles.Badge}`} onClick={() => setCategory("turkish")}>Turkish</Badge>
+              <Badge variant="secondary" pill className={`${styles.Badge}`} onClick={() => setCategory("french")}>French</Badge>
+              <Badge variant="secondary" pill className={`${styles.Badge}`} onClick={() => setCategory("british")}>British</Badge>
+              <Badge variant="secondary" pill className={`${styles.Badge}`} onClick={() => setCategory("german")}>German</Badge>
+              <Badge variant="secondary" pill className={`${styles.Badge}`} onClick={() => setCategory("austrian")}>Austrian</Badge>
+              <Badge variant="secondary" pill className={`${styles.Badge}`} onClick={() => setCategory("lebanese")}>Lebanese</Badge>
+              <Badge variant="secondary" pill className={`${styles.Badge}`} onClick={() => setCategory("moroccan")}>Moroccan</Badge>
+              <Badge variant="secondary" pill className={`${styles.Badge}`} onClick={() => setCategory("caribbean")}>Caribbean</Badge>
+              <Badge variant="secondary" pill className={`${styles.Badge}`} onClick={() => setCategory("indian")}>Indian</Badge>
+              <Badge variant="secondary" pill className={`${styles.Badge}`} onClick={() => setCategory("thai")}>Thai</Badge>
+              <Badge variant="secondary" pill className={`${styles.Badge}`} onClick={() => setCategory("japanese")}>Japanese</Badge>
+              <Badge variant="secondary" pill className={`${styles.Badge}`} onClick={() => setCategory("chinese")}>Chinese</Badge>
+              <Badge variant="secondary" pill className={`${styles.Badge}`} onClick={() => setCategory("mexican")}>Mexican</Badge>
+              <Badge variant="secondary" pill className={`${styles.Badge}`} onClick={() => setCategory("american")}>American</Badge>
+              <Badge variant="secondary" pill className={`${styles.Badge}`} onClick={() => setCategory("other")}>Other</Badge>
           </Container>
         </Col>
 
