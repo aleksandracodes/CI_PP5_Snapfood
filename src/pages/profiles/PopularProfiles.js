d6161e0d724624472af037143f792da6555d2fb7
Aleksandra <a.haniok@gmail.com>
2022-07-23 07:26:44 +0000

    Fetch and display profile names from the API

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -1,13 +1,50 @@
-import React from 'react'
-import { Container } from 'react-bootstrap';
+import React, { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
+import { axiosReq } from "../../api/axiosDefaults";
 import appStyles from "../../App.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import Asset from "../../components/Asset";
 
 const PopularProfiles = () => {
-  return (
-    <Container className={appStyles.Content}>
-        <p>Top Users</p>
-    </Container>
-  )
-}
+  const [profileData, setProfileData] = useState({
+    pageProfile: { results: [] },
+    popularProfiles: { results: [] },
+  });
+  const { popularProfiles } = profileData;
+  const currentUser = useCurrentUser();
 
-export default PopularProfiles
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const { data } = await axiosReq.get(
+          "/profiles/?ordering=-followers_count" // descending order meaning the most followed user will be on the top
+        );
+        setProfileData((prevState) => ({
+          ...prevState,
+          popularProfiles: data,
+        }));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    handleMount();
+  }, [currentUser]);
+
+  return (
+    <Container className={`${appStyles.Content} font-weight-bold`}>
+      {popularProfiles.results.length ? (
+        <>
+          <p>Top Users</p>
+          {popularProfiles.results.map((profile) => (
+            <p key={profile.id}>{profile.owner}</p>
+          ))}
+        </>
+      ) : (
+        <Asset spinner />
+      )}
+    </Container>
+  );
+};
+
+export default PopularProfiles;
