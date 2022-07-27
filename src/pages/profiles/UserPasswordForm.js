8defc8542824ccf542a4a001f9299d38f073dfdb
Aleksandra <a.haniok@gmail.com>
2022-07-27 15:54:03 +0000

    Add the page to change user password & create PasswordCriteria component

---
+++ src/pages/profiles/UserPasswordForm.js
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from "react";
+import { Alert, Button, Col, Container, Form, Row } from "react-bootstrap";
+import { useHistory, useParams } from "react-router-dom";
+import { axiosRes } from "../../api/axiosDefaults";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import appStyles from "../../App.module.css";
+import PasswordCriteria from "../../components/PasswordCriteria";
+
+const UserPasswordForm = () => {
+  const history = useHistory();
+  const { id } = useParams();
+  const currentUser = useCurrentUser();
+
+  const [userData, setUserData] = useState({
+    new_password1: "",
+    new_password2: "",
+  });
+  const { new_password1, new_password2 } = userData;
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (event) => {
+    setUserData({
+      ...userData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  useEffect(() => {
+    if (currentUser?.profile_id?.toString() !== id) {
+      history.push("/");
+    }
+  }, [currentUser, history, id]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axiosRes.post("/dj-rest-auth/password/change/", userData);
+      history.goBack();
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
+
+  return (
+    <Row>
+      <Col className="py-2 mx-auto text-center font-weight-bold" md={8}>
+        <Container className={appStyles.Content}>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group>
+              <Form.Label>New password</Form.Label>
+              <Form.Control
+                placeholder="type your new password"
+                type="password"
+                value={new_password1}
+                onChange={handleChange}
+                name="new_password1"
+                className={`${appStyles.Input} text-center`}
+              />
+            </Form.Group>
+            {errors?.new_password1?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+            <Form.Group>
+              <Form.Label>Confirm password</Form.Label>
+              <Form.Control
+                placeholder="confirm new password"
+                type="password"
+                value={new_password2}
+                onChange={handleChange}
+                name="new_password2"
+                className={`${appStyles.Input} text-center`}
+              />
+
+            <PasswordCriteria />
+
+            </Form.Group>
+            {errors?.new_password2?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+            <Button
+              type="submit"
+              className={`mx-2 my-2 ${appStyles.button}`}
+              onMouseDown={(event) => event.preventDefault()}
+              >
+              Save
+            </Button>
+            <Button
+              onMouseDown={(event) => event.preventDefault()}
+              className={`mx-2 ${appStyles.button}`}
+              onClick={() => history.goBack()}>
+              Cancel
+            </Button>
+          </Form>
+        </Container>
+      </Col>
+    </Row>
+  );
+};
+
+export default UserPasswordForm;
