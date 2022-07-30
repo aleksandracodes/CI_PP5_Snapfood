545a48aebcf664c504abaa9f333234ab48e8d72e
Aleksandra <a.haniok@gmail.com>
2022-07-30 07:18:39 +0000

    Correct the import of NavLink from bootstrap to remove warning from console

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Card, Button, Row, Col, Container } from "react-bootstrap";
-import NavLink from "react-router-dom/NavLink";
+import { NavLink } from "react-router-dom";
 import landingImage from "../assets/landing-page-image.png";
 import styles from "../styles/Landing.module.css";
 import appStyles from "../App.module.css";
